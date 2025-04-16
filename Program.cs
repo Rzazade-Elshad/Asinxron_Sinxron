f2c37b2cdfa2a39486c544acf6b16439392fd080
RzazadeElshad <elshadrr-bp218@code.edu.az>
2025-04-16 11:50:30 +0400

    asyn

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Diagnostics;
+
+namespace asinxron_sinxron;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        List<string> urls = new List<string>
+        {
+            "https://www.translate.com",
+            "https://www.chatgpt.com",
+            "https://www.github.com",
+            "https://www.stackoverflow.com",
+            "https://www.Barcelona.com"
+        };
+        GetData(urls).Wait();
+    }
+        static async Task GetData(List<string> urls)
+        {
+            using HttpClient client = new HttpClient(); 
+
+            Stopwatch stopwatch = new Stopwatch(); 
+            stopwatch.Start(); 
+
+            foreach (string url in urls)
+            {
+                    HttpResponseMessage response = await client.GetAsync(url); 
+
+                    if (response.IsSuccessStatusCode) 
+                    {
+                        Console.WriteLine($"Request sent  {url}");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Request failedd {url} - {response.StatusCode}");
+                    }
+            }
+
+            stopwatch.Stop(); 
+            Console.WriteLine(stopwatch.Elapsed.TotalSeconds);
+        }
+    }
